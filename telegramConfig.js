9368ae340aeda6ee7722b66111c6ea894fe555ad
Vikas Rajput <39953644+Vikas-Rajput-JS@users.noreply.github.com>
2025-07-18 11:47:41 +0530

    Update telegramConfig.js

--- telegramConfig.js
+++ telegramConfig.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const getChatId = async () => {
   let resp = await axios.get(
-    `https://api.telegram.org/${process.env.BOT_TOKEN}/getUpdates`
+    `https://api.telegram.org/bot${process.env.BOT_TOKEN}/getUpdates`
   );
   console.log(resp?.data?.result[0]?.message?.from?.id);
   return resp?.data?.result[0]?.message?.from?.id;
@@ -13,7 +13,7 @@ const getChatId = async () => {
 const sendAlert = async (message = "Hello") => {
   let chatId = await getChatId();
   let resp = await axios.post(
-    `https://api.telegram.org/${process.env.BOT_TOKEN}/sendMessage`,
+    `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
     {
       chat_id: chatId,
       text: message,
